5908ca75b97b51c2ce8728c6b7c0793c1e7a26d0
joanapailanan <pailananjoanamae1@gmail.com>
2025-03-29 12:53:19 +0800

    TO DEPLOY

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,86 @@
+<?php
+ namespace App\Http\Controllers;
+ use Illuminate\Http\Request;
+ use App\Models\User;
+ use App\Models\UserJob;
+ use App\Traits\ApiResponser;
+ use Illuminate\Http\Response;
+ 
+ Class UserController extends Controller {
+
+    use ApiResponser;
+    private $request;
+    public function __construct(Request $request){
+        $this->request = $request;
+    }
+    public function getUsers(){
+        //eloquent style
+        $users = User::all();
+        return response()->json($users, 200);
+    }
+
+    public function index()
+    {
+        $users = User::all();
+        return $this->successResponse($users);
+        
+    }
+
+    public function add(Request $request ){
+        $rules = [
+            'username' => 'required|max:20',
+            'password' => 'required|max:20',
+            'gender' => 'required|in:Male,Female',
+            'jobid' => 'required|numeric|min:1|not_in:0',
+        ];
+        $this->validate($request,$rules);
+
+        // validate if Jobid is found in the table tbluserjob
+        $userjob = UserJob::findOrFail($request->jobid);
+        $user = User::create($request->all());
+        return $this->successResponse($user, Response::HTTP_CREATED);
+    }
+
+    public function show($id)
+    {
+         $user = User::findOrFail($id);
+         return $this->successResponse($user);
+         
+ }
+ public function update(Request $request,$id)
+    {
+        $rules = [
+        'username' => 'max:20',
+        'password' => 'max:20',
+        'gender' => 'in:Male,Female',
+        'jobid' => 'required|numeric|min:1|not_in:0',
+        ];
+        $this->validate($request, $rules);
+
+        // validate if Jobid is found in the table tbluserjob
+        $userjob =UserJob::findOrFail($request->jobid);
+        
+        $user = User::findOrFail($id);
+            
+        $user->fill($request->all());
+        // if no changes happen
+        if ($user->isClean()) {
+            return $this->errorResponse('At least one value must change', Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+        $user->save();
+        return $this->successResponse($user);
+       
+    }
+       /**
+     * Remove an existing user
+     * @return Illuminate\Http\Response
+     */
+    public function delete($id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+
+        return $this->successResponse($user);
+
+    }
+ }
